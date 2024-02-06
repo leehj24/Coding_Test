7829040ed16e2b6a234976caf40bd7f2c59718cd
hyeonji.lee <hyeonji.lee@hlcompany.com>
2024-02-06 12:14:34 +0900

    level1,2

---
+++ Level.1_py/시저암호.py
@@ -0,0 +1,12 @@
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+
+    return "".join(s)
+
+#"AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다
+#문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
