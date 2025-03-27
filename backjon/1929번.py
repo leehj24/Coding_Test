50ebda970e43f982be5a33b5daa8e3bddb4c00a5
leehj24 <hyunji4579@gmail.com>
2025-03-27 12:03:07 +0900

    backjon

---
+++ backjon/1929번.py
@@ -0,0 +1,13 @@
+n = list(map(int, input().split()))
+print(n)
+answer = []
+for i in range(n[0], n[1] + 1):
+    if i < 2:  # 1은 소수가 아님
+        continue
+    is_prime = True
+    for j in range(2, int(i**0.5) + 1):  # i의 제곱근까지만 확인
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i)
